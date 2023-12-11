da9706a4b94284867c013b1d3ca1ff90655bb6f0
ConnextMarketing <support@connext.marketing>
2023-12-11 20:39:13 +0000

    Created AI Assistant Componenet

---
+++ src/app/ai-assistant/ai-assistant.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ai-assistant',
+  templateUrl: './ai-assistant.component.html',
+  styleUrls: ['./ai-assistant.component.scss'],
+})
+export class AiAssistantComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
