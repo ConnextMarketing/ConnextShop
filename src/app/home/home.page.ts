0a5dd94eeb9d396fe13e47e4266344e29bc98b01
C4rr13rX <C4rr13rX@gmail.com>
2023-12-11 18:54:36 -0500

    update src/app/home/home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonButton, IonTitle, IonMenuButton, IonButtons, IonMenu, IonContent, IonList, IonItem } from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, bookOutline, bookSharp, briefcaseOutline, buildOutline, codeSlashOutline, codeSlashSharp, colorPaletteOutline, colorPaletteSharp, chatbubblesOutline, chatbubblesSharp, scanCircle, backspace, refreshCircle, closeCircleOutline, addCircleOutline, newspaperOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +16,11 @@ import { IonicModule } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    
+    addIcons({ chatbubblesOutline, chatbubblesSharp, colorPaletteOutline, colorPaletteSharp, paperPlaneOutline, bookOutline, bookSharp, briefcaseOutline, codeSlashOutline, codeSlashSharp, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, scanCircle, backspace, refreshCircle, closeCircleOutline, addCircleOutline, newspaperOutline, buildOutline });
+    
+  }
 
   ngOnInit() {
   }
