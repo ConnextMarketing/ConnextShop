af331717a707d37812b9e1686529879eaa49bee9
C4rr13rX <C4rr13rX@gmail.com>
2023-12-13 16:44:39 -0500

    update src/app/services/environment.service.ts

--- src/app/services/environment.service.ts
+++ src/app/services/environment.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnvironmentService {
+  
+  private environmentUrl = 'assets/environment.json'; // Path to your JSON file
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getEnvironment(): Observable<any> {
+    return this.http.get(this.environmentUrl);
+  }
 }
