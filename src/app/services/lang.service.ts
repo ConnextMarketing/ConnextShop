a2e6552081f0171652b486ca2ce1005e4f43142b
C4rr13rX <C4rr13rX@gmail.com>
2023-12-18 16:32:53 -0500

    update src/app/services/lang.service.ts

--- src/app/services/lang.service.ts
+++ src/app/services/lang.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { LangConfig } from '../interfaces/lang-config'
+import { LangConfig } from '../interfaces/lang-config';
 
 @Injectable({
   providedIn: 'root'
