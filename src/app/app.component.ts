27a8cd41c68683c199f35531482a9daa9eb56e57
C4rr13rX <C4rr13rX@gmail.com>
2023-12-20 19:07:36 -0500

    update src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, buildOutline, buildSharp, folderOpenOutline, folderOpenSharp, eyeOutline, eyeSharp, callOutline, callSharp } from 'ionicons/icons';
-import { LangService, LangConfig } from './services/lang.service';
+import { LangService  } from './services/lang.service';
+import { LangConfig } from './interfaces/lang-config';
 
 interface AppPage {
   title: string;
