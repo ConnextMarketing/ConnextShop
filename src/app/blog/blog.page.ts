98c70bd574450eeca8bd5fd51872633f3f5b8911
C4rr13rX <C4rr13rX@gmail.com>
2023-12-21 19:10:56 -0500

    update src/app/blog/blog.page.ts

--- src/app/blog/blog.page.ts
+++ src/app/blog/blog.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
@@ -19,9 +19,26 @@ export class BlogPage implements OnInit {
   currentLanguage: string = 'eng'; // this could be dynamically set
   public title!: string;
 
-  constructor() { }
+  constructor(
+    private langService: LangService,
+    private changeDetectorRef: ChangeDetectorRef
+    ) { }
 
   ngOnInit() {
+    this.langService.getEnvironment().subscribe(
+  config => {
+    this.langConfig = config;
+    // Ensure that the properties exist in langConfig before accessing them
+    const getTranslation = (key: string) => this.langConfig[key] ? this.langConfig[key][this.currentLanguage] : 'Unknown';
+    this.title = getTranslation('blog');
+    this.changeDetectorRef.detectChanges();
+  },
+  error => {
+    // Handle the error and alert the error message
+    this.changeDetectorRef.detectChanges();
+    alert('Error: ' + error.message);
+  }
+);
   }
 
 }
